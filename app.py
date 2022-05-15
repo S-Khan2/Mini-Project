c0e669d1fa970492f18cb75648dd6a4f493c3f82
Sahl Khan <skg.skhan2@gmail.com>
2022-05-16 00:21:16 +0100

    Improve user experience by introducing a logo, having a loop switching between menus, and adding a time delay when returning to main menu

--- app.py
+++ app.py
@@ -1,40 +1,91 @@
-# Aim: To write a easy-to-follow refactored code
+# Aim: To improve user experience from earlier version of app.py from Week 1
 
-products = ['Coke Zero', 'Fanta', 'Tea', 'Coffee']
-product_menu_options = ['Main Menu', 'Print Product List', 'Create New Product',
-                        'Update Existing Product', 'Delete Product']
+import os       # Helpful to clear terminal
+import time     # Helpful to add waiting times
+from logo import logo     # Collects my logo from 'logo.py'
 
-def get_main_menu_option():
-    return input('Enter 0 to exit or 1 to open product menu: ')
+# Create list of products
+products = [
+    'Coke Zero',
+    'Fanta', 
+    'Tea', 
+    'Coffee'
+    ]
 
-def get_product_menu_option():
-    print('Enter a number for each product:')
-    for option in enumerate(product_menu_options):
-        print(f'{option}')
-    return input()
+product_menu_options = [
+    'Main Menu', 
+    'Print Product List', 
+    'Create New Product',
+    'Update Existing Product', 
+    'Delete Product'
+    ]
 
+# Clear terminal
+def clear_console():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
+
+# Clear terminal and print logo
+def display_logo():
+    clear_console()
+    print(logo)
+
+# Close application after a delay
+def close_app():
+    display_logo()
+    print('Closing application...')
+    # Thought: Now we could store the product list update
+    time.sleep(1)
+    clear_console()
+    print('Application closed')
+    exit()
+
+# Take a list, find max length, find how many can fit per line,
+# keep the items aligned
+def multi_print(our_list, line_length):
+    return None
+
+# Display main menu and return input
+def get_main_menu_choice():
+    display_logo()
+    print('Main Menu:\n  [0]  Exit Application\n  [1]  Product Menu')
+    return input('Enter option: ')
+
+# Display product menu and return option
+def get_product_menu_choice():
+    display_logo()
+    print('Product Menu:')
+    for i, option in enumerate(product_menu_options):
+        print(f'  [{i}]  {option}')
+    return input('Enter option: ')
+
+# Print products with index, and return index as an int
 def get_product_index():
-    for product in enumerate(products):
-            print(f'{product}')
+    for i, product in enumerate(products):
+            print(f'  [{i}]  {product}')
     return int(input('Enter index of product: '))
 
-user_input = get_main_menu_option()
-if user_input == '0':
-    exit()
-else:
-    user_input = get_product_menu_option()
-    if user_input == '0':
-        user_input = get_main_menu_option()
-    elif user_input == '1':
-        print(products)
-    elif user_input == '2':
-        products.append(input('Enter new product: '))
-        print(products)
-    elif user_input == '3':
-        index = get_product_index()
-        products[index] = input('Enter update of product: ')
-        print(products)
-    elif user_input == '4':
-        del products[get_product_index()]
-        print(products)
+# Run until we exit app
+while True:
+    user_input = get_main_menu_choice()
+    if user_input == '0': # exit
+        close_app()
+    else:
+        user_input = get_product_menu_choice()
+        if user_input == '2': # add new product
+            new_product = input('Enter new product: ').title()
+            products.append(new_product)
+        elif user_input == '3': # update old product
+            index = get_product_index()
+            products[index] = input('Enter update of product: ')
+        elif user_input == '4': # delete product by index
+            del products[get_product_index()]
+        if user_input != '0': # print products list
+            print(f'List of products: {products}')
         
+        # Add two seconds delay
+        time.sleep(1)
+        print('\n  Loading main menu...')
+        time.sleep(1)
