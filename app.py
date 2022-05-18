bd325381ff6ca16a2c61d1e4377ea8ae4b2a095e
Sahl Khan <skg.skhan2@gmail.com>
2022-05-18 22:53:30 +0100

    Save on exit, and ensure main code runs only within app.py

--- app.py
+++ app.py
@@ -129,45 +129,48 @@ def display_logo():
     print(logo)
 
 # Close application after a delay
-def close_app():
+def close_app(menus: list[Menu]):
     display_logo()
     print('Closing application...')
     # Thought: Now we could store the product list update
+    for menu in menus[1:]:
+        menu.save()
     time.sleep(1)
     clear_terminal()
     print('Application closed')
     exit()
 
 # App runs from here onwards
-main_menu = Menu('main', main_menu_options, True)
-product_menu = ItemsMenu('product', product_menu_options, PRODUCTS_FILE_NAME)
-courier_menu = ItemsMenu('courier', couriers_menu_options, COURIERS_FILE_NAME)
-menus = (main_menu, product_menu, courier_menu) # irreplacable objects 
-menu_index = 0
+if __name__ == "__main__":
+    main_menu = Menu('main', main_menu_options, True)
+    product_menu = ItemsMenu('product', product_menu_options, PRODUCTS_FILE_NAME)
+    courier_menu = ItemsMenu('courier', couriers_menu_options, COURIERS_FILE_NAME)
+    menus = (main_menu, product_menu, courier_menu) # irreplacable objects 
+    menu_index = 0
 
-while menu_index != None:
-    current_menu = menus[menu_index]
-    current_option = current_menu.get_option()
-    if menu_index == 0 and current_option == 0:
-        main_menu.is_active = False
-    elif menu_index == 0:
-        current_menu.switch_to(menus[current_option])
-    elif current_option == 0:
-        current_menu.switch_to(main_menu)
-    elif current_option == 1:
-        current_menu.print_items(False, 0.5, 2)
-    elif current_option == 2:
-        current_menu.append_item()
-        current_menu.save()
-    elif current_option == 3:
-        current_menu.update_item()
-        current_menu.save()
-    elif current_option == 4:
-        current_menu.delete_item()
-        current_menu.save()
+    while menu_index != None:
+        current_menu = menus[menu_index]
+        current_option = current_menu.get_option()
+        if menu_index == 0 and current_option == 0:
+            main_menu.is_active = False
+        elif menu_index == 0:
+            current_menu.switch_to(menus[current_option])
+        elif current_option == 0:
+            current_menu.switch_to(main_menu)
+        elif current_option == 1:
+            current_menu.print_items(False, 0.5, 2)
+        elif current_option == 2:
+            current_menu.append_item()
+            current_menu.save()
+        elif current_option == 3:
+            current_menu.update_item()
+            current_menu.save()
+        elif current_option == 4:
+            current_menu.delete_item()
+            current_menu.save()
 
-    menu_index = get_active(menus)
-    if menu_index != None:
-        menus[menu_index].delay_loading_menu(1.5)
+        menu_index = get_active(menus)
+        if menu_index != None:
+            menus[menu_index].delay_loading_menu(1.5)
 
-close_app()     
+    close_app(menus)     
