c69587af1a55574c4c141080a844099bd7549f68
Sahl Khan <skg.skhan2@gmail.com>
2022-05-19 10:03:15 +0100

    Read/write list of dict from/into JSON

--- file_handler.py
+++ file_handler.py
@@ -1,7 +1,9 @@
 # file_handler.py
 # Read/write a list of str from/to a textfile
+# TODO: Read/write a list of dict from/into a CSV file
+import json
 
-def read_file(file_name: str) -> list[str]:
+def read_txtfile(file_name: str) -> list[str]:
     try:
         with open(file_name, 'r') as f:
             return [line.strip() for line in f.readlines()]
@@ -11,7 +13,7 @@ def read_file(file_name: str) -> list[str]:
         # Raising an exception isn't a good idea
 
 
-def write_file(file_name: str, our_list: list[str]):
+def write_textfile(file_name: str, our_list: list[str]):
     try:
         with open(file_name, 'w') as f:
             for item in our_list:
@@ -19,3 +21,16 @@ def write_file(file_name: str, our_list: list[str]):
     except Exception as e:
         print(e)
 
+def read_jsonfile(file_name: str) -> list[dict]:
+    try:
+        with open(file_name, 'r') as f:
+            return json.load(f)
+    except:
+        return []
+
+def write_jsonfile(file_name: str, json_list: list[dict]):
+    try:
+        with open(file_name, 'w') as json_file:
+            json.dump(json_list, json_file, indent=4) # human readable json file
+    except Exception as e:
+        print(e)
