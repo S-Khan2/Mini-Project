9981f3120039ed63730ebc57d59fd92e945e8d4e
Sahl Khan <skg.skhan2@gmail.com>
2022-07-25 12:13:04 +0100

    Added related functions to logo module

--- logo.py
+++ logo.py
@@ -1,3 +1,8 @@
+# logo.py
+# This module handles the refreshing of the screen and the logo design
+
+import os
+
 logo = '''____________________________________________________
 
                            (       
@@ -13,3 +18,17 @@ logo = '''____________________________________________________
         `-.________,-'               `-..___..-'  
 ____________________________________________________
 '''
+
+def clear_terminal():
+    '''Clear terminal, independant of operation system'''
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
+
+
+def display_logo(): 
+    '''Clear terminal and print logo'''
+    clear_terminal()
+    # return None # Temporarily do not print logo
+    print(logo)
